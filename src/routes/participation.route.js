17043398eaf9ea71642c87ceeae00982794933d9
ThibWill <thibault.willer@gmail.com>
2019-01-17 12:36:23 +0100

    change variable for lolo

--- src/routes/participation.route.js
+++ src/routes/participation.route.js
@@ -54,8 +54,8 @@ router.route('/usersInterestedEvent').get(async (req, res) => {
   if (!idEvent || typeof idEvent !== 'string') { return res.status(400).send({ err: 'idEvent is not defined' }); }
 
   try {
-    const users = await dbService.userInterrestedEvent(idEvent);
-    return res.status(200).send({ users });
+    const event = await dbService.userInterrestedEvent(idEvent);
+    return res.status(200).send({ event });
   } catch (e) {
     console.error(e);
     return res.status(500).send({ err: 'Error append when getting the event' });
